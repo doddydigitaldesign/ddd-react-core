b692fbc38970e5e32e56035b1592ceb7f79bded1
Daniel Sonne Lehnberg <doddy@doddy.se>
2021-02-09 21:04:05 +0100

    Update Input, add Checkbox

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,13 +1,44 @@
-import React, { InputHTMLAttributes, useState } from "react";
+import React, { InputHTMLAttributes, useRef, useState } from "react";
 import classNames from "../../lib/utils/classNames";
 import styles from "./Input.module.css";
-
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps
+    extends Omit<InputHTMLAttributes<HTMLInputElement>, "value"> {
     label: string;
     id: string;
+    value?: string | ReadonlyArray<string> | number | boolean;
 }
 
-export const Input: React.FC<InputProps> = ({ label, className, ...rest }) => {
+export const Input: React.FC<InputProps> = ({
+    label,
+    className,
+    value,
+    checked,
+    ...rest
+}) => {
+    const ref = useRef<HTMLInputElement>(null);
+    const checkMark = (
+        <svg
+            className={classNames(styles.checkMark, {
+                [styles.checkMarkVisible]: checked,
+                [styles.checkMarkHidden]: !checked,
+            })}
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            onClick={(e) => {
+                if (ref.current) {
+                    ref.current.click();
+                }
+            }}
+        >
+            {checked ? (
+                <path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>
+            ) : (
+                <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path>
+            )}
+        </svg>
+    );
+
     const [validityState, setValidityState] = useState<ValidityState | null>(
         null,
     );
@@ -42,6 +73,7 @@ export const Input: React.FC<InputProps> = ({ label, className, ...rest }) => {
                 {label}
             </label>
             <input
+                ref={ref}
                 title={validityStateErrorKey}
                 onChange={(e) => {
                     setValidityState(e.target.validity);
@@ -63,8 +95,11 @@ export const Input: React.FC<InputProps> = ({ label, className, ...rest }) => {
                     },
                     className,
                 )}
+                checked={rest.type === "checkbox" ? checked : undefined}
+                value={typeof value === "string" ? value : undefined}
                 {...rest}
             />
+            {rest.type === "checkbox" && checkMark}
         </div>
     );
 };
